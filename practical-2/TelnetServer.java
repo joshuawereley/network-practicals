4f4c10193651458d3bac7b5c7036c13836b5cedb
Joshua Wereley <joshuawereley@icloud.com>
2025-02-14 10:19:15 +0200

    Initialize socket and input stream

---
+++ practical-2/TelnetServer.java
@@ -0,0 +1,10 @@
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TelnetServer {
+
+    private Socket s = null;
+    private ServerSocket ss = null;
+    private DataInputStream in = null;
+}
