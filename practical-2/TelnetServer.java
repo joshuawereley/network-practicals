612fa0c3fbe9f111af3f093f1b6d6da9b879186a
Joshua Wereley <joshuawereley@icloud.com>
2025-02-14 10:39:40 +0200

    Construct with port
    Starts a new server and waits for a connection.

--- practical-2/TelnetServer.java
+++ practical-2/TelnetServer.java
@@ -1,4 +1,6 @@
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,4 +9,35 @@ public class TelnetServer {
     private Socket s = null;
     private ServerSocket ss = null;
     private DataInputStream in = null;
+
+    public TelnetServer(int port) {
+        try {
+            ss = new ServerSocket(port);
+            System.out.println("Server has started");
+            System.out.println("Wating for a client ...");
+
+            s = ss.accept();
+            System.out.println("Client accepted");
+
+            in = new DataInputStream(
+                new BufferedInputStream(s.getInputStream())
+            );
+
+            String m = "";
+
+            while (!m.equals("Over")) {
+                try {
+                    m = in.readUTF();
+                    System.out.println(m);
+                } catch (IOException i) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+            s.close();
+            in.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
 }
