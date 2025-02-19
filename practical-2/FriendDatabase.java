a9aaf72c6f2ed7582d050e38b8929c0780c5c407
Joshua Wereley <joshuawereley@icloud.com>
2025-02-19 20:13:38 +0200

    Add storage hashmap and constructor
    I have a hashmap to store the name of the person and the phone number of that person.
    I have also decided to make the phone number of type String as the person might type 072 or +27 72.
    The constructor creates the storage object.

---
+++ practical-2/FriendDatabase.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class FriendDatabase {
+
+    HashMap<String, String> storage;
+
+    public FriendDatabase() {
+        storage = new HashMap<String, String>();
+    }
+}
