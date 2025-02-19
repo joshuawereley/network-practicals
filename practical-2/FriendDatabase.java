1f6f897edc04cbd8603900f89fc275c8efa19081
Joshua Wereley <joshuawereley@icloud.com>
2025-02-19 21:14:21 +0200

    Add saveContacts and writeToFile

--- practical-2/FriendDatabase.java
+++ practical-2/FriendDatabase.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -25,6 +27,35 @@ public class FriendDatabase {
             reader.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error handling file: " + e.getMessage());
+        }
+    }
+
+    public void saveContacts() {
+        try {
+            File file = new File("friends.txt");
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+                writeToFile(file.getName());
+            } else {
+                System.out.println("File already exists");
+                writeToFile(file.getName())
+            }
+        } catch (IOException e) {
+            System.out.println("Error creating file: " + e.getMessage());
+        }
+    }
+
+    public void writeToFile(String fileName) {
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            for (String i : storage.keySet()) {
+                //TODO: NEED TO FORMAT BETTER WHEN WRITING TO FILE
+                writer.write(i + "," + storage.get(i));
+            }
+            writer.close();
+            System.out.println("Successfully wrote to " + fileName);
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
         }
     }
 }
