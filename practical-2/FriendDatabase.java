ef0cdb5c1033eb4838e68dcda0d8626083b18ba4
Joshua Wereley <joshuawereley@icloud.com>
2025-02-19 20:29:03 +0200

    Add loadContacts function
    This function loads contacts from a file and stores all the contacts in the storage hashmap.

--- practical-2/FriendDatabase.java
+++ practical-2/FriendDatabase.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class FriendDatabase {
 
@@ -6,5 +9,22 @@ public class FriendDatabase {
 
     public FriendDatabase() {
         storage = new HashMap<String, String>();
+    }
+
+    public void loadContacts() {
+        try {
+            File file = new File("friends.txt");
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                String[] splitData = data.split(",");
+                String name = splitData[0];
+                String number = splitData[1];
+                storage.put(name, number);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error handling file: " + e.getMessage());
+        }
     }
 }
