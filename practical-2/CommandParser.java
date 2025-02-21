339416253bbecd27b96d76e883a75ac7b36a9eab
Joshua Wereley <joshuawereley@icloud.com>
2025-02-21 11:42:22 +0200

    Add CommandParser constructor
    Provides a shallow copy of an existing databse.

---
+++ practical-2/CommandParser.java
@@ -0,0 +1,8 @@
+public class CommandParser {
+
+    private FriendDatabase database = new FriendDatabase();
+
+    public CommandParser(FriendDatabase database) {
+        this.database = database;
+    }
+}
