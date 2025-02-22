5a4f76423108d8c1ed4eb2db3589b95844d1d445
Joshua Wereley <joshuawereley@icloud.com>
2025-02-22 20:25:26 +0200

    Update CommandParser
    Here are the following things I have updated:
    * Validated if the command is empty or not.
    * Made sure that the action is automatically converted to upper case to prevent
    uneccessary problems.
    * For all of the cases I increased the length check by one to include surnames.
    Furthermore, I have updated the usages to now include surnames.
    * I have updated the HELP command output so that users have a better
    understanding as to what each command does.

--- practical-2/CommandParser.java
+++ practical-2/CommandParser.java
@@ -1,6 +1,6 @@
 public class CommandParser {
 
-    private FriendDatabase database = new FriendDatabase();
+    private FriendDatabase database;
 
     public CommandParser(FriendDatabase database) {
         this.database = database;
@@ -8,33 +8,62 @@ public class CommandParser {
 
     public String processCommand(String command) {
         String[] splitProcess = command.split(" ");
-        switch (command) {
+        if (splitProcess.length == 0) return "Invalid command!";
+        switch (splitProcess[0].toUpperCase()) {
             case "ADD":
-                if (splitProcess.length == 3) {
-                    database.addContact(splitProcess[1], splitProcess[2]);
-                    return "Contact added successfully!";
+                if (splitProcess.length == 4) {
+                    database.addContact(
+                        splitProcess[1],
+                        splitProcess[2],
+                        splitProcess[3]
+                    );
+                    return (
+                        splitProcess[1] +
+                        " " +
+                        splitProcess[2] +
+                        " is successfully added!"
+                    );
                 } else {
-                    return "Usage: ADD <name> <phonenumber>";
+                    return "Usage: ADD <name> <surname> <phonenumber>";
                 }
             case "SEARCH":
                 if (splitProcess.length == 2) {
-                    String result = database.searchContact(splitProcess[1]);
+                    String result = database.searchContact(
+                        splitProcess[1],
+                        splitProcess[2]
+                    );
                     return result;
                 } else {
-                    return "Usage: SEARCH <name>";
+                    return "Usage: SEARCH <name> <surname>";
                 }
             case "DELETE":
-                if (splitProcess.length == 2) {
-                    database.deleteContact(splitProcess[1]);
-                    return "Contact deleted successfully!";
+                if (splitProcess.length == 3) {
+                    database.deleteContact(splitProcess[1], splitProcess[2]);
+                    return (
+                        splitProcess[1] +
+                        " " +
+                        splitProcess[2] +
+                        " is successfully deleted!"
+                    );
                 } else {
-                    return "Usage: DELETE <name>";
+                    return "Usage: DELETE <name> <surname>";
                 }
             case "LIST":
                 database.listContacts();
                 return "Listed contacts successfully!";
             case "HELP":
-                return "Available commands: ADD, SEARCH, DELETE, LIST, EXIT";
+                String commands = "Available commands:\n";
+                commands +=
+                    "ADD <name> <surname> <phoneNumber>  - Add a new friend";
+                commands +=
+                    "DELETE <name> <surname>             - Remove a friend";
+                commands +=
+                    "SEARCH <name> <surname>             - Find a friend's phone number";
+                commands +=
+                    "LIST                                - Show all saved contacts";
+                commands +=
+                    "EXIT                                - Disconnect from the server";
+                return commands;
             case "EXIT":
                 return "Goodbye!";
             default:
