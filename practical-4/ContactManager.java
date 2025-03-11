5fcd48c465a487623bcb667f5ea1cc02aae25ecd
Joshua Wereley <joshuawereley@icloud.com>
2025-03-11 10:13:05 +0200

    Add getAllContacts function
    It retrieves all stored contacts and formats them as an HTML string for display in a browser.

--- practical-4/ContactManager.java
+++ practical-4/ContactManager.java
@@ -26,4 +26,24 @@ public class ContactManager {
     }
     return false;
   }
+
+  public String getAllContacts() {
+    if (!contacts.isEmpty()) {
+      StringBuilder response = new StringBuilder();
+      response.append("<h2>Contact List</h2>");
+      response.append("<table border='1'>");
+      response.append("<tr><th>Name</th><th>Phone Number</th></tr>");
+
+      for (String i : contacts.keySet()) {
+        response.append("<tr>");
+        response.append("<td>").append(i).append("</td>");
+        response.append("<td>").append(contacts.get(i)).append("</td>");
+        response.append("</tr>");
+      }
+
+      response.append("</table>");
+      return response.toString();
+    }
+    return "<h3>No contacts available.</h3>";
+  }
 }
