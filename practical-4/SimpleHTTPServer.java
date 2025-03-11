4a6866eadaa1ad002cd614b32d0e27646b441b7b
Joshua Wereley <joshuawereley@icloud.com>
2025-03-11 10:21:42 +0200

    Change port to static final int

--- practical-4/SimpleHTTPServer.java
+++ practical-4/SimpleHTTPServer.java
@@ -8,13 +8,15 @@ import java.net.InetSocketAddress;
 
 public class SimpleHTTPServer {
   
+  private static final int PORT 8080;
+
   public static void main(String[] args) {
-    InetSocketAddress socketAddress = new InetSocketAddress(8080);
+    InetSocketAddress socketAddress = new InetSocketAddress(PORT);
     HttpServer server = HttpServer.create(socketAddress, 0);
     server.createContext("/", new MyHandler());
     server.setExecutor(null);
     server.start();
-    System.out.println("Server is running on port 8080");
+    System.out.println("Server is running on port " + PORT);
   }
 
 }
