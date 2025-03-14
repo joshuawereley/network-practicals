1ff9e5ed10de729f4295217558c6e712f5c7eb81
Joshua Wereley <joshuawereley@icloud.com>
2025-03-14 15:01:37 +0200

    Add parseQueryParameters and part of run functions
    The purpose of the parseQueryParameters function is to extract key-value pairs from the query string of a URL in an HTTP request.
    The purpose of the run function includes:
    * Starting the server; listening for incoming HTTP requests on a specified port.
    * Handling client connections: it waits for and processes each request.
    * Ensuring the server runs continously: it typically runs in a loop to handle multiple requests.

--- practical-4/ClientHandler.java
+++ practical-4/ClientHandler.java
@@ -1,4 +1,9 @@
 import java.net.Socket;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.OutputStream;
 
 public class ClientHandler implements Runnable {
   private Socket socket;
@@ -9,5 +14,41 @@ public class ClientHandler implements Runnable {
     this.contactManager = contactManager;
   }
 
+  @Override
+  public void run() {
+    try {
+      InputSteam inputStream = socket.getInputStream();
+      InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+      BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+      OutputStream outputStream = socket.getOutputStream();
+      String line = bufferedReader.readLine();
+      if (line != null && !line.isEmpty() {
+        System.out.println("Received: " + line);
+        String[] tokens = line.split(" ");
+        if (tokens.length >= 2) {
+          String method = tokens[0];
+          String path = tokens[1];
+          HasMap<String, String> parameters = parseQueryParameters(path);
+        }
+      }
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public HashMap<String, String> parseQueryParameters(String path) {
+    HashMap<String, String> hashMap = new HashMap<String, String>();
+    int queryIndex = path.indexOf('?');
+    if (queryIndex == -1)
+      return hashMap;
+    String query = path.substring(queryIndex + 1);
+    for (String pair : query.split("&")) {
+      String[] keyValue = pair.split("=", 2);
+      if (keyValue.length == 2)
+        hashMap.put(keyValue[0], keyValue[1];
+    }
+
+    return hashMap;
+  } 
 
 }
