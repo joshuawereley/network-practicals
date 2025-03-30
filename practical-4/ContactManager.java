2ca3288e6b5b1b86d180732351343ea038cf1f86
Joshua Wereley <joshuawereley@icloud.com>
2025-03-30 13:48:43 +0200

    Update ContactManager

--- practical-4/ContactManager.java
+++ practical-4/ContactManager.java
@@ -1,90 +1,99 @@
-import java.util.HashMap;
-import java.util.Base64;
-import java.net.URLEncoder;
-import java.net.URLDecoder;
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Base64;
+import java.util.HashMap;
 
 public class ContactManager {
 
-  private HashMap<String, String> contacts;
-  private HashMap<String, byte[]> contactImages;
+    private HashMap<String, String> contacts;
+    private HashMap<String, byte[]> contactImages;
 
-  public ContactManager() {
-    contacts = new HashMap<String, String>();
-    contactImages = new HashMap<String, byte[]>();
-  }
+    public ContactManager() {
+        contacts = new HashMap<>();
+        contactImages = new HashMap<>();
 
-  public void addContact(String name, String number) {
-    if (!name.isEmpty() && !number.isEmpty()) {
-      contacts.put(name, number);
+        // Add some sample data
+        contacts.put("John Doe", "123-456-7890");
+        contacts.put("Jane Smith", "987-654-3210");
     }
-  }
 
-  public void addContactImage(String name, byte[] imageData) {
-    if (!name.isEmpty() && imageData != null) {
-      contactImages.put(name, imageData);
+    public void addContact(String name, String number) {
+        if (!name.isEmpty() && !number.isEmpty()) {
+            contacts.put(name, number);
+        }
     }
-  }
 
-  public String getContact(String name) {
-    if (!name.isEmpty() && contacts.containsKey(name)) {
-      return contacts.get(name);
+    public String getContact(String name) {
+        return contacts.get(name);
     }
-    return null;
-  }
 
-  public byte[] getContactImage(String name) {
-    if (!name.isEmpty()) {
-      return contactImages.get(name);
+    public boolean deleteContact(String name) {
+        if (contacts.containsKey(name)) {
+            contacts.remove(name);
+            contactImages.remove(name); // Also remove associated image
+            return true;
+        }
+        return false;
     }
-    return null;
-  }
 
-  public boolean deleteContact(String name) {
-    if (!name.isEmpty() && contacts.containsKey(name)) {
-      contacts.remove(name);
-      contactImages.remove(name);
-      return true;
+    public void addContactImage(String name, byte[] imageData) {
+        if (!name.isEmpty() && imageData != null) {
+            contactImages.put(name, imageData);
+        }
     }
-    return false;
-  }
 
-  public String getAllContacts() {
-    StringBuilder response = new StringBuilder();
-    if (contact.isEmpty()) {
-      response.append("<p>No contacts available.</p>");
-    } else {
-      response.append("<table border='1' style='width:100%'>");
-      response.append("<tr><th>Name</th><th>Phone No.</th><th>Action</th></tr>");
-
-      for (String i : contacts.keySet()) {
-        response.append("<tr>");
-        response.append("<td>").append(escapeHtml(i)).append("</td>");
-        response.append("<td>").append(escapeHtml(contacts.get(i))).append("</td>");
-        response.append("<td><a href='/deleteContact?name=");
-        response.append(encodeUrl(i)).append("'>Delete</a>");
-        response.append(" | <a href'/image?name=").append(encodeUrl(i));
-        response.append("'>View Image</a></td>");
-        response.append("</tr>");
-      }
-      response.append("</table>");
-      return response.toString();
+    public byte[] getContactImage(String name) {
+        return contactImages.get(name);
     }
-  }
 
-  public String escapeHtml(String input) {
-    return input.replace("&", "&amp;")
-                .replace("<", "&lt;")
-                .replace(">", "&gt;")
-                .replace("\"", "&quot;")
-                .replace("'", "&#39;");
-  }
+    public String getAllContactsHTML() {
+        StringBuilder sb = new StringBuilder();
 
-  public String encodeUrl(String input) {
-    try {
-      return URLEncoder.encode(input, "UTF-8");
-    } catch (UnsupportedEncodingException e) {
-      return input;
+        if (contacts.isEmpty()) {
+            sb.append("<p>No contacts available.</p>");
+        } else {
+            sb.append("<table border='1' style='width:100%'>");
+            sb.append("<tr><th>Name</th><th>Phone</th><th>Action</th></tr>");
+
+            for (String name : contacts.keySet()) {
+                sb.append("<tr>");
+                sb.append("<td>").append(escapeHtml(name)).append("</td>");
+                sb
+                    .append("<td>")
+                    .append(escapeHtml(contacts.get(name)))
+                    .append("</td>");
+                sb
+                    .append("<td><a href='/deleteContact?name=")
+                    .append(encodeUrl(name))
+                    .append("'>Delete</a>");
+                sb
+                    .append(" | <a href='/image?name=")
+                    .append(encodeUrl(name))
+                    .append("'>View Image</a></td>");
+                sb.append("</tr>");
+            }
+
+            sb.append("</table>");
+        }
+
+        return sb.toString();
     }
-  }
+
+    private String escapeHtml(String input) {
+        return input
+            .replace("&", "&amp;")
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("\"", "&quot;")
+            .replace("'", "&#39;");
+    }
+
+    private String encodeUrl(String input) {
+        try {
+            return URLEncoder.encode(input, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return input; // fallback
+        }
+    }
 }
