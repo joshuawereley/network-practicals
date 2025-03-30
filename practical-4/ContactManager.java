7ce079e191434f02e2b42aa330ef84b590a074f8
Joshua Wereley <joshuawereley@icloud.com>
2025-03-30 13:06:45 +0200

    Update ContactManager
    Here is a list of changes made:
    * Added image support
    * Improved HTML generation
    * URL encoding & decoding
    * Error Handling

--- practical-4/ContactManager.java
+++ practical-4/ContactManager.java
@@ -1,4 +1,8 @@
 import java.util.HashMap;
+import java.util.Base64;
+import java.net.URLEncoder;
+import java.net.URLDecoder;
+import java.io.UnsupportedEncodingException;
 
 public class ContactManager {
 
@@ -11,41 +15,76 @@ public class ContactManager {
   }
 
   public void addContact(String name, String number) {
-    if (!name.isEmpty() && !number.isEmpty())
+    if (!name.isEmpty() && !number.isEmpty()) {
       contacts.put(name, number);
+    }
+  }
+
+  public void addContactImage(String name, byte[] imageData) {
+    if (!name.isEmpty() && imageData != null) {
+      contactImages.put(name, imageData);
+    }
   }
 
   public String getContact(String name) {
-    if (!name.isEmpty() && contacts.containsKey(name))
+    if (!name.isEmpty() && contacts.containsKey(name)) {
       return contacts.get(name);
+    }
+    return null;
+  }
+
+  public byte[] getContactImage(String name) {
+    if (!name.isEmpty()) {
+      return contactImages.get(name);
+    }
     return null;
   }
 
   public boolean deleteContact(String name) {
     if (!name.isEmpty() && contacts.containsKey(name)) {
       contacts.remove(name);
+      contactImages.remove(name);
       return true;
     }
     return false;
   }
 
   public String getAllContacts() {
-    if (!contacts.isEmpty()) {
-      StringBuilder response = new StringBuilder();
-      response.append("<h2>Contact List</h2>");
-      response.append("<table border='1'>");
-      response.append("<tr><th>Name</th><th>Phone Number</th></tr>");
+    StringBuilder response = new StringBuilder();
+    if (contact.isEmpty()) {
+      response.append("<p>No contacts available.</p>");
+    } else {
+      response.append("<table border='1' style='width:100%'>");
+      response.append("<tr><th>Name</th><th>Phone No.</th><th>Action</th></tr>");
 
       for (String i : contacts.keySet()) {
         response.append("<tr>");
-        response.append("<td>").append(i).append("</td>");
-        response.append("<td>").append(contacts.get(i)).append("</td>");
+        response.append("<td>").append(escapeHtml(i)).append("</td>");
+        response.append("<td>").append(escapeHtml(contacts.get(i))).append("</td>");
+        response.append("<td><a href='/deleteContact?name=");
+        response.append(encodeUrl(i)).append("'>Delete</a>");
+        response.append(" | <a href'/image?name=").append(encodeUrl(i));
+        response.append("'>View Image</a></td>");
         response.append("</tr>");
       }
-
       response.append("</table>");
       return response.toString();
     }
-    return "<h3>No contacts available.</h3>";
+  }
+
+  public String escapeHtml(String input) {
+    return input.replace("&", "&amp;")
+                .replace("<", "&lt;")
+                .replace(">", "&gt;")
+                .replace("\"", "&quot;")
+                .replace("'", "&#39;");
+  }
+
+  public String encodeUrl(String input) {
+    try {
+      return URLEncoder.encode(input, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      return input;
+    }
   }
 }
