6380cbc5e1cc86ea9e22c73b24069c13d3a06dbc
Joshua Wereley <joshuawereley@icloud.com>
2025-03-31 15:24:51 +0200

    Add Constructor for POP3ClientService

---
+++ practical-7/POP3ClientService.java
@@ -0,0 +1,13 @@
+public class POP3ClientService {
+  private Socket socket;
+  private BufferedReader input;
+  private OutputStreamWriter output;
+
+  public POP3ClientService(String server, int port) {
+    if (!server.isEmpty()) {
+      this.socket = new Socket(server, port);
+      InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+      this.input = new BufferedReader(isr);
+      this.output = new OutputStreamWriter(socket.getOutputStream());
+    }
+}
